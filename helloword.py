b5340ab55764e40ca09868f9b66ed85d7bc896a5
idsglee <idsglee>
2018-07-25 16:35:28 +0900

    실습파일추가

--- helloword.py
+++ helloword.py
@@ -1,1 +1,8 @@
+a = 10
+b = "hello"
 print("helloword")
+print(a)
+print(b)
+c = 20
+print(a+c)
+ 
---
+++ list.py
@@ -0,0 +1,8 @@
+fruit = ['apple','banana', 'watermelon']
+print(fruit)
+print(fruit[0])
+fruit.append('orange')
+print(fruit)
+del fruit[1]
+print(fruit)
+
