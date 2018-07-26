cd1c4074ee945004337eb80e76b3b1fe15c83ec9
idsglee <idsglee>
2018-07-26 10:49:06 +0900

    추가올릴께

---
+++ input.py
@@ -0,0 +1,21 @@
+# name = input("what id your name?")
+# print("hello, " + name)
+
+print("Sum >")
+num1 = input("first number : ")
+num2 = input("Second number : ")
+num1 = int(num1)
+num2 = int(num2)
+print(num1+num2)
+
+# 빼기, 곱하기, 나누기, 나머지
+print(num1-num2)
+print(num1*num2)
+print(num1/num2) #몫을 반환
+print(num1%num2) #나머지를 반환
+
+if (num1%2 == 0):
+    print("num1 is even")
+else:
+    print("num1 is odd" ) 
+       
---
+++ while.py
@@ -0,0 +1,9 @@
+fruits = ['apple','banana', 'kiwi']
+
+for fruit in fruits:
+    print(fruit + "가 있습니다.")
+
+index = 0
+while index<2:
+    print(fruits[index] + "를 꺼냈습니다.")
+    index = index + 1
